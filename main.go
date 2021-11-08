6b127af40f4a8ce46a541ce089a03ce43144be75
ZhangYinze <1285763952@qq.com>
2021-11-08 12:22:57 +0800

    fix: change the password for database connection

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 
-	db, _ := gorm.Open("mysql", "root:123456@tcp(headless-service-port-0:3306)/test?charset=utf8&parseTime=True&loc=Local")
+	db, _ := gorm.Open("mysql", "root:3306@tcp(headless-service-port-0:3306)/test?charset=utf8&parseTime=True&loc=Local")
 	db.AutoMigrate(&sql.User{})
 	db.AutoMigrate(&sql.Envelope{})
 	db.Close()
