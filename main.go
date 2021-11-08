a72ea4404683fb31de23e357fd2b80328ad045dd
ZhangYinze <1285763952@qq.com>
2021-11-08 13:22:18 +0800

    fix: modify database connection information

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 
 	db, _ := gorm.Open("mysql", "root:3306@tcp(172.27.11.82:3306)/test?charset=utf8&parseTime=True&loc=Local")
-	defer db.Close()
+	//defer db.Close()
 	db.AutoMigrate(&sql.User{})
 	db.AutoMigrate(&sql.Envelope{})
 
